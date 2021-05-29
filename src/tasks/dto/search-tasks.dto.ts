1fa616e26ea513fe2eaee1fbcc8d48a70fae74eb
earth <virudson@gmail.com>
2021-05-29 17:45:27 +0700

    add validation for search params

--- src/tasks/dto/search-tasks.dto.ts
+++ src/tasks/dto/search-tasks.dto.ts
@@ -1,6 +1,10 @@
+import { IsEnum, IsNotEmpty } from 'class-validator';
 import { TaskStatuses } from '../task.model';
 
 export class SearchTasksDto {
+  @IsEnum(TaskStatuses)
   status?: TaskStatuses;
+
+  @IsNotEmpty()
   search?: string;
 }
